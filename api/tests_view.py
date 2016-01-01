6a45fecd129f5760b660dbe8f7554d5365129799
Jongwook Kim <fantazic@gmail.com>
2016-01-01 15:39:32 +0900

    remove unnecessary test cases change api urls

--- api/tests_view.py
+++ api/tests_view.py
@@ -9,37 +9,37 @@ class ApiScenarioTests(TestCase):
         client = Client()
 
         # check me first
-        response = client.get('/api/me/')
+        response = client.get('/tms/api/me/')
         self.assertContains(response, 'Logout')
 
         # login fails
-        response = client.post('/api/login/', {'username': 'tester', 'password': 'tester'})
+        response = client.post('/tms/api/login/', {'username': 'tester', 'password': 'tester'})
         self.assertContains(response, 'Wrong Username')
 
         # register a user
-        response = client.post('/api/signup/', {'username': 'tester', 'password': 'tester'})
+        response = client.post('/tms/api/signup/', {'username': 'tester', 'password': 'tester'})
         self.assertContains(response, 'Success')
 
         # try login again and succeed
-        response = client.post('/api/login/', {'username': 'tester', 'password': 'tester'})
+        response = client.post('/tms/api/login/', {'username': 'tester', 'password': 'tester'})
         self.assertContains(response, 'Success')
 
         # try me again
-        response = client.get('/api/me/')
+        response = client.get('/tms/api/me/')
         self.assertContains(response, 'Login')
 
         data = json.loads(response.content)
         self.assertEquals(data['user']['username'], 'tester')
 
         # get tasks, empty
-        response = client.get('/api/get_tasks/')
+        response = client.get('/tms/api/get_dates_and_tasks/')
         self.assertContains(response, 'Success')
 
         data = json.loads(response.content)
         self.assertEquals(len(data['tasks']), 0)
 
         # add a task
-        response = client.post('/api/add_task/', {'note': 'TEST NOTE', 'date': '2015-01-01', 'hour': '1'})
+        response = client.post('/tms/api/crud_task/', {'action': 'Create', 'note': 'TEST NOTE', 'date': '2015-01-01', 'hour': '1'})
         self.assertContains(response, 'Success')
 
         data = json.loads(response.content)
@@ -47,21 +47,21 @@ class ApiScenarioTests(TestCase):
         self.assertEquals(data['tasks'][0]['note'], 'TEST NOTE')
 
         # get tasks on the proper date
-        response = client.get('/api/get_tasks/', {'date': '2015-01-01'})
+        response = client.get('/tms/api/get_dates_and_tasks/', {'date': '2015-01-01'})
         self.assertContains(response, 'Success')
 
         data = json.loads(response.content)
         self.assertEquals(len(data['tasks']), 1)
 
         # get tasks on another date, empty
-        response = client.get('/api/get_tasks/', {'date': '2015-01-02'})
+        response = client.get('/tms/api/get_dates_and_tasks/', {'date': '2015-01-02'})
         self.assertContains(response, 'Success')
 
         data = json.loads(response.content)
         self.assertEquals(len(data['tasks']), 0)
 
         # edit the task
-        response = client.post('/api/update_task/',
+        response = client.post('/tms/api/crud_task/',
                                {'task_id': '1', 'note': 'TEST NOTE Updated', 'date': '2015-01-01', 'hour': '1',
                                 'action': 'Edit'})
         self.assertContains(response, 'Success')
@@ -71,7 +71,7 @@ class ApiScenarioTests(TestCase):
         self.assertEquals(data['tasks'][0]['note'], 'TEST NOTE Updated')
 
         # delete the task
-        response = client.post('/api/update_task/',
+        response = client.post('/tms/api/crud_task/',
                                {'task_id': '1', 'note': 'TEST NOTE Updated', 'date': '2015-01-01', 'hour': '1',
                                 'action': 'Delete'})
         self.assertContains(response, 'Success')
